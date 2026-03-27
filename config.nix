f5f7e898a0fa7a1a56caa93a919012d9440137e6
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-28 00:20:32 +0100

    [~] Delete power management

--- config.nix
+++ config.nix
@@ -26,14 +26,6 @@
     };
 
     services.thermald.enable = true;
-    services.power-profiles-daemon.enable = false;
-    services.tlp = {
-        enable = true;
-        settings = {
-            CPU_SCALING_GOVERNOR_ON_AC = "performance";
-            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        };
-    };
     powerManagement.enable = true;
 
     time.timeZone = "Europe/Paris";
