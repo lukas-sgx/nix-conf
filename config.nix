bd61c675214416b117581116d251d2f973347cba
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-28 00:42:06 +0100

    [~] Change driver name

--- config.nix
+++ config.nix
@@ -20,7 +20,7 @@
         extraPackages = with pkgs; [
             intel-media-driver
             intel-vaapi-driver
-            vaapiVdpau
+            libva-vdpau-driver
             libvdpau-va-gl
         ];
     };
