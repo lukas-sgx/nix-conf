18643864232f944b6e73aec51e9c2ab205036eae
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-28 19:52:39 +0100

    [~] Change verion user

--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 {
     home.username = "lukas";
     home.homeDirectory = "/home/lukas";
-    home.stateVersion = "24.11";
+    home.stateVersion = "25.05";
 
     home.packages = (with pkgs; [
         vscode
