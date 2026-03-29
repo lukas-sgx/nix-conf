e5f93f31e7e3ae16d72203199f62bb29b1c480c0
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-29 20:49:57 +0200

    [+] Add exegol

--- setup.sh
+++ setup.sh
@@ -2,19 +2,19 @@
 
 #shell install
 tide configure --auto \
-        --style=Rainbow \
-        --prompt_colors='True color' \
-        --show_time='24-hour format' \
-        --rainbow_prompt_separators=Round \
-        --powerline_prompt_heads=Round \
-        --powerline_prompt_tails=Round \
-        --powerline_prompt_style='Two lines, character' \
-        --prompt_connection=Disconnected \
-        --powerline_right_prompt_frame=No \
-        --prompt_spacing=Sparse \
-        --icons='Many icons' \
-        --transient=No
+    --style=Rainbow \
+    --prompt_colors='True color' \
+    --show_time='24-hour format' \
+    --rainbow_prompt_separators=Round \
+    --powerline_prompt_heads=Round \
+    --powerline_prompt_tails=Round \
+    --powerline_prompt_style='Two lines, character' \
+    --prompt_connection=Disconnected \
+    --powerline_right_prompt_frame=No \
+    --prompt_spacing=Sparse \
+    --icons='Many icons' \
+    --transient=No
 
 #exegol install
-python3 -m venv ~/exegol-venv
-~/exegol-venv/bin/pip install exegol
+python3 -m venv ~/.exegol-venv
+~/.exegol-venv/bin/pip install exegol
