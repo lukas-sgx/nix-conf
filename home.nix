eed02b56d723da4642c403dcf236ed7b25b6e6a1
lukas-sgx <lukas.soigneux@epitech.eu>
2026-03-29 17:32:08 +0200

    [~] Set kind & git alias

--- home.nix
+++ home.nix
@@ -1,156 +1,158 @@
 { config, pkgs, zen-browser, system, lib, ... }:
 {
-  home.username = "lukas";
-  home.homeDirectory = "/home/lukas";
-  home.stateVersion = "25.05";
+    home.username = "lukas";
+    home.homeDirectory = "/home/lukas";
+    home.stateVersion = "25.05";
 
-  home.packages = (with pkgs; [
-    libgtop
-    vscode
-    llvmPackages_20.clang
-    docker-compose
-    go
-    gnome-extension-manager
-    firefox
-    discord
-    eza
-    fzf
-    kind
-    kubectl
-    zoxide
-    bat
-  ]) ++ [
-    zen-browser.packages.${system}.default
-  ];
-
-  programs.git = {
-    enable = true;
-    settings = {
-      user.name        = "lukas-sgx";
-      user.email       = "lukas.soigneux@epitech.eu";
-      init.defaultBranch = "main";
-      pull.rebase      = false;
-    };
-  };
-
-  programs.fish = {
-    enable = true;
-
-    shellAliases = {
-      ll     = "eza -la --icons --git --group-directories-first";
-      lt     = "eza --tree --icons --level=2";
-      ls     = "eza --icons";
-      cat    = "bat --style=auto";
-      nrs    = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
-      nfu    = "cd ~/epitech/free/nix-conf && nix flake update && cd -";
-      nfu-switch = "nfu && nrs";
-      k      = "kubectl";
-      ".."   = "cd ..";
-      "..."  = "cd ../..";
-      exegol = "~/.exegol-venv/bin/exegol";
-    };
-
-    plugins = [
-      { name = "tide";     src = pkgs.fishPlugins.tide.src; }
-      { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+    home.packages = (with pkgs; [
+        libgtop
+        vscode
+        llvmPackages_20.clang
+        docker-compose
+        go
+        gnome-extension-manager
+        firefox
+        discord
+        eza
+        fzf
+        kind
+        kubectl
+        zoxide
+        bat
+    ]) ++ [
+        zen-browser.packages.${system}.default
     ];
 
-    interactiveShellInit = ''
-      set -g fish_greeting ""
-      zoxide init fish | source
+    programs.git = {
+        enable = true;
+        settings = {
+        user.name        = "lukas-sgx";
+        user.email       = "lukas.soigneux@epitech.eu";
+        init.defaultBranch = "main";
+        pull.rebase      = false;
+        };
+    };
 
-      # Catppuccin Mocha — syntax fish
-      set -g fish_color_normal          cdd6f4
-      set -g fish_color_command         89b4fa
-      set -g fish_color_keyword         cba6f7
-      set -g fish_color_quote           a6e3a1
-      set -g fish_color_redirection     f5c2e7
-      set -g fish_color_end             fab387
-      set -g fish_color_error           f38ba8
-      set -g fish_color_param           cdd6f4
-      set -g fish_color_comment         6c7086
-      set -g fish_color_autosuggestion  6c7086
-      set -g fish_color_operator        f5c2e7
-      set -g fish_color_escape          f2cdcd
-      set -g fish_pager_color_progress  cba6f7
+    programs.fish = {
+        enable = true;
 
-      # Tide — OS
-      set -U tide_os_bg_color           cba6f7
-      set -U tide_os_color              1e1e2e
-      set -U tide_os_icon               '󱄅'
+        shellAliases = {
+        ll     = "eza -la --icons --git --group-directories-first";
+        lt     = "eza --tree --icons --level=2";
+        ls     = "eza --icons";
+        cat    = "bat --style=auto";
+        nrs    = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
+        nfu    = "cd ~/epitech/free/nix-conf && nix flake update && cd -";
+        nfu-switch = "nfu && nrs";
+        g = "git";
+        kb     = "kubectl";
+        ki     = "kind"; 
+        ".."   = "cd ..";
+        "..."  = "cd ../..";
+        exegol = "~/.exegol-venv/bin/exegol";
+        };
 
-      # Tide — PWD
-      set -U tide_pwd_bg_color          89b4fa
-      set -U tide_pwd_bg_color_anchors  89b4fa
-      set -U tide_pwd_color_anchors     1e1e2e
-      set -U tide_pwd_color_dirs        1e1e2e
-      set -U tide_pwd_color_truncated_dirs 313244
-      set -U tide_pwd_icon              '󰉋'
-      set -U tide_pwd_icon_home         '󱂵'
+        plugins = [
+        { name = "tide";     src = pkgs.fishPlugins.tide.src; }
+        { name = "fzf-fish"; src = pkgs.fishPlugins.fzf-fish.src; }
+        ];
 
-      # Tide — Git clean
-      set -U tide_git_bg_color          a6e3a1
-      set -U tide_git_color_branch      1e1e2e
-      set -U tide_git_color_clean       1e1e2e
-      set -U tide_git_icon              '󰊢'
+        interactiveShellInit = ''
+        set -g fish_greeting ""
+        zoxide init fish | source
 
-      # Tide — Git dirty
-      set -U tide_git_bg_color_unstaged f9e2af
-      set -U tide_git_color_dirty       1e1e2e
-      set -U tide_git_color_staged      1e1e2e
-      set -U tide_git_color_untracked   1e1e2e
-      set -U tide_git_color_ahead       1e1e2e
-      set -U tide_git_color_behind      1e1e2e
-      set -U tide_git_color_conflicted  f38ba8
+        # Catppuccin Mocha — syntax fish
+        set -g fish_color_normal          cdd6f4
+        set -g fish_color_command         89b4fa
+        set -g fish_color_keyword         cba6f7
+        set -g fish_color_quote           a6e3a1
+        set -g fish_color_redirection     f5c2e7
+        set -g fish_color_end             fab387
+        set -g fish_color_error           f38ba8
+        set -g fish_color_param           cdd6f4
+        set -g fish_color_comment         6c7086
+        set -g fish_color_autosuggestion  6c7086
+        set -g fish_color_operator        f5c2e7
+        set -g fish_color_escape          f2cdcd
+        set -g fish_pager_color_progress  cba6f7
 
-      # Tide — Durée commandes
-      set -U tide_cmd_duration_bg_color 313244
-      set -U tide_cmd_duration_color    fab387
-      set -U tide_cmd_duration_icon     '󱦟'
-      set -U tide_cmd_duration_threshold 0
-      set -U tide_cmd_duration_decimals 2
+        # Tide — OS
+        set -U tide_os_bg_color           cba6f7
+        set -U tide_os_color              1e1e2e
+        set -U tide_os_icon               '󱄅'
 
-      # Tide — Heure
-      set -U tide_time_bg_color         313244
-      set -U tide_time_color            6c7086
-      set -U tide_time_icon             '󰥔'
+        # Tide — PWD
+        set -U tide_pwd_bg_color          89b4fa
+        set -U tide_pwd_bg_color_anchors  89b4fa
+        set -U tide_pwd_color_anchors     1e1e2e
+        set -U tide_pwd_color_dirs        1e1e2e
+        set -U tide_pwd_color_truncated_dirs 313244
+        set -U tide_pwd_icon              '󰉋'
+        set -U tide_pwd_icon_home         '󱂵'
 
-      # Tide — Status
-      set -U tide_status_icon           '󰄬'
-      set -U tide_status_icon_failure   '󰅚'
+        # Tide — Git clean
+        set -U tide_git_bg_color          a6e3a1
+        set -U tide_git_color_branch      1e1e2e
+        set -U tide_git_color_clean       1e1e2e
+        set -U tide_git_icon              '󰊢'
 
-      # Tide — Outils
-      set -U tide_go_icon               '󰟓'
-      set -U tide_go_bg_color           94e2d5
-      set -U tide_go_color              1e1e2e
-      set -U tide_kubectl_icon          '󱃾'
-      set -U tide_kubectl_bg_color      cba6f7
-      set -U tide_kubectl_color         1e1e2e
-      set -U tide_node_icon             '󰎙'
-      set -U tide_python_icon           '󰌠'
-      set -U tide_docker_icon           '󰡨'
-      set -U tide_nix_shell_icon        '󱄅'
-      set -U tide_nix_shell_bg_color    89b4fa
-      set -U tide_nix_shell_color       1e1e2e
-      set -U tide_jobs_icon             '󰑮'
+        # Tide — Git dirty
+        set -U tide_git_bg_color_unstaged f9e2af
+        set -U tide_git_color_dirty       1e1e2e
+        set -U tide_git_color_staged      1e1e2e
+        set -U tide_git_color_untracked   1e1e2e
+        set -U tide_git_color_ahead       1e1e2e
+        set -U tide_git_color_behind      1e1e2e
+        set -U tide_git_color_conflicted  f38ba8
 
-      # Tide — Curseur
-      set -U tide_prompt_color_success  a6e3a1
-      set -U tide_prompt_color_failure  f38ba8
-      set -U tide_character_icon        '❯'
-      set -U tide_character_vi_icon_default '❯'
-      set -U tide_prompt_icon_connection ' '
-      set -U tide_prompt_connection_enabled false
+        # Tide — Durée commandes
+        set -U tide_cmd_duration_bg_color 313244
+        set -U tide_cmd_duration_color    fab387
+        set -U tide_cmd_duration_icon     '󱦟'
+        set -U tide_cmd_duration_threshold 0
+        set -U tide_cmd_duration_decimals 2
 
-      # FZF
-      set -gx FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1e1e2e,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --border rounded --prompt '❯ '"
-    '';
-  };
+        # Tide — Heure
+        set -U tide_time_bg_color         313244
+        set -U tide_time_color            6c7086
+        set -U tide_time_icon             '󰥔'
 
-  programs.ssh = {
-    enable = true;
-    matchBlocks."*".addKeysToAgent = "yes";
-  };
+        # Tide — Status
+        set -U tide_status_icon           '󰄬'
+        set -U tide_status_icon_failure   '󰅚'
 
-  programs.home-manager.enable = true;
+        # Tide — Outils
+        set -U tide_go_icon               '󰟓'
+        set -U tide_go_bg_color           94e2d5
+        set -U tide_go_color              1e1e2e
+        set -U tide_kubectl_icon          '󱃾'
+        set -U tide_kubectl_bg_color      cba6f7
+        set -U tide_kubectl_color         1e1e2e
+        set -U tide_node_icon             '󰎙'
+        set -U tide_python_icon           '󰌠'
+        set -U tide_docker_icon           '󰡨'
+        set -U tide_nix_shell_icon        '󱄅'
+        set -U tide_nix_shell_bg_color    89b4fa
+        set -U tide_nix_shell_color       1e1e2e
+        set -U tide_jobs_icon             '󰑮'
+
+        # Tide — Curseur
+        set -U tide_prompt_color_success  a6e3a1
+        set -U tide_prompt_color_failure  f38ba8
+        set -U tide_character_icon        '❯'
+        set -U tide_character_vi_icon_default '❯'
+        set -U tide_prompt_icon_connection ' '
+        set -U tide_prompt_connection_enabled false
+
+        # FZF
+        set -gx FZF_DEFAULT_OPTS "--color=bg+:#313244,bg:#1e1e2e,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 --border rounded --prompt '❯ '"
+        '';
+    };
+
+    programs.ssh = {
+        enable = true;
+        matchBlocks."*".addKeysToAgent = "yes";
+    };
+
+    programs.home-manager.enable = true;
 }
