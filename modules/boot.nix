7ae9c319095c964446a2b7c904c02431753b0d4a
lukas-sgx <68616614+lukas-sgx@users.noreply.github.com>
2026-06-19 21:50:46 +0200

    feat: improve security

--- modules/boot.nix
+++ modules/boot.nix
@@ -1,6 +1,16 @@
 { self, pkgs, inputs, lib, config, ... }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 2;
-  boot.kernelPackages = pkgs.linuxPackages;
+    services.fwupd.enable = true;
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.configurationLimit = 2;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+    
+    security.auditd.enable = true;
+    security.audit.enable = true;
+    boot.kernel.sysctl = {
+        "kernel.kptr_restrict" = 2;
+        "kernel.dmesg_restrict" = 1;
+        "net.ipv4.conf.all.rp_filter" = 1;
+  };
 }
