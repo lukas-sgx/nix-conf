6e40f98efd5a3b2c4448ae46c69e5573360c5d9f
lukas-sgx <68616614+lukas-sgx@users.noreply.github.com>
2026-06-19 21:53:50 +0200

    feat: set firewall & randomize address & encrypted dns

--- modules/network.nix
+++ modules/network.nix
@@ -2,8 +2,21 @@
 let
     env = import ../env.nix;
 in {
-  networking.hostName = env.hostName;
-  networking.networkmanager.enable = true;
-  networking.networkmanager.dns = "none";
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    networking.firewall.enable = true;
+    networking.hostName = env.hostName;
+    networking.networkmanager.enable = true;
+    
+    services.resolved = {
+        enable = true;
+        settings.Resolve = {
+            dnssec = "true";
+            dnsovertls = "true";
+            MulticastDNS = "no";
+            LLMNR = "no";
+        };
+    };
+    networking.nameservers = [ "1.1.1.1#cloudflare-dns.com" "1.0.0.1#cloudflare-dns.com" ];
+    
+    networking.networkmanager.wifi.macAddress = "stable";
+    networking.networkmanager.ethernet.macAddress = "stable";
 }
