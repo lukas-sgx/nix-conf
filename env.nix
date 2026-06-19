937b91dafe887f2aa4f961713f9cd2348a68f06f
lukas-sgx <68616614+lukas-sgx@users.noreply.github.com>
2026-06-19 21:49:35 +0200

    feat: improve privacy

--- env.nix
+++ env.nix
@@ -1,9 +1,9 @@
 {
     username = "lukas";
     homeDir = "/home/lukas";
-    gitEmail = "lukas.soigneux@epitech.eu";
+    gitEmail = "68616614+lukas-sgx@users.noreply.github.com";
     gitUser = "lukas-sgx";
-    gpgSign = "19F45BF088FD1431";
+    gpgSign = "296DAB34361F2C06B00F7C36010918459327D84A";
     descriptionName = "Lukas";
     hostName = "nixos";
     nixVersion = "26.05";
